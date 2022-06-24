ecba02a76a67e3d4b1b37227cd0018bb84dba747
Nick Baez <w0m85k27@protonmail.com>
2022-06-24 15:39:14 -0400

    second commit, homepage loads

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const PORT = process.env.PORT || 4000;
+const app = require("./app");
+
+const init = async () => {
+  try {
+    app.listen(PORT, () => console.log(`WELCOME DEGEN TO PORT: ${PORT}`));
+  } catch (ex) {
+    console.log(ex);
+  }
+};
+
+init();
